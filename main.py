f5d914809d851220da4763dd1e3228ca22f76b89
ali015421 <ali015421@gmail.com>
2026-08-03 04:56:41 +0500

    Add negative income validation, clean up decimal output with int()

--- main.py
+++ main.py
@@ -2,27 +2,29 @@
 #first version of tax  calculator for  Pakistani users.
 
 income= int(input("HELLO, Enter Your Income:"));
+if income >= 0:
+    if income <= 600000:
+        tax_owed= 0;
+        print ("You owe no tax")
 
-if income <= 600000:
-     tax_owed= 0;
-     print ("You owe no tax")
+    elif income <= 1200000:
+        tax_owed = (income-600000)/100;
+        print ("The tax you owe is:", int(tax_owed))
 
-elif income <= 1200000:
-     tax_owed = (income-600000)/100;
-     print ("The tax you owe is:", tax_owed)
+    elif income <= 2200000:
+        tax_owed = 6000+((income-1200000)/100)*11 ;
+        print ("The tax you owe is:",int(tax_owed))
 
-elif income <= 2200000:
-     tax_owed = 6000+((income-1200000)/100)*11 ;
-     print ("The tax you owe is:",tax_owed)
+    elif income <= 3200000:
+        tax_owed = 116000+((income-2200000)/100)*23;
+        print ("The tax you owe is:", int(tax_owed))
 
-elif income <= 3200000:
-     tax_owed = 116000+((income-2200000)/100)*23;
-     print ("The tax you owe is:", tax_owed)
+    elif income <= 4100000:
+        tax_owed = 346000+((income-3200000)/100)*30;
+        print ("The tax you owe is:", int(tax_owed))
 
-elif income <= 4100000:
-     tax_owed = 346000+((income-3200000)/100)*30;
-     print ("The tax you owe is:",tax_owed)
-
+    else:
+        tax_owed = 616000+((income-4100000)/100)*35 ; 
+        print ("The tax you owe is:",int(tax_owed))
 else:
-     tax_owed = 616000+((income-4100000)/100)*35 ; 
-     print ("The tax you owe is:",tax_owed)
+    print("Income cannot be negative")
